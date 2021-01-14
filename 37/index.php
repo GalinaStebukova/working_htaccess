41691eee75439aace232aa6d9833b21c41d4105d
GalinaStebukova <Stebukova18@yandex.ru>
2021-01-14 21:41:00 +0700

    Add task 37

---
+++ 37/index.php
@@ -0,0 +1,9 @@
+<?php
+    error_reporting(E_ALL);
+    ini_set('display_errors', 'on');
+
+    include 'test/test.php';
+
+//    echo 'index<br>';
+//    var_dump($_SERVER['REQUEST_URI']);
+//    var_dump($_GET['get']);
