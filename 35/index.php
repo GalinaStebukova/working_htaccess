938c1f694ba55fcccc2485005d2d980f69030127
GalinaStebukova <Stebukova18@yandex.ru>
2021-01-14 21:33:46 +0700

    Add task 35

---
+++ 35/index.php
@@ -0,0 +1,8 @@
+<?php
+    error_reporting(E_ALL);
+    ini_set('display_errors', 'on');
+
+    echo 'index<br>';
+    var_dump($_SERVER['REQUEST_URI']);
+//    echo '<br>';
+//    var_dump($_GET['get']);
